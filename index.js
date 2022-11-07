25b9b1bf73c35fe08e4398fb1ef5a1c7564443d0
Pelin Karakoca <karakocapelin@gmail.com>
2022-11-07 18:53:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => response.json())
+  .then((json) => renderBooks(json));
   // To pass the tests, don't forget to return your fetch!
   
 }
